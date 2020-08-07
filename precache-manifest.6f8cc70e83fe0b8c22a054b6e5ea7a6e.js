77d6e18e723db182d678bc84d6c0a2b55dda282d
Rico Metzger <rico.metzger@gmail.com>
2020-08-07 14:34:56 +0200

    Updates

---
+++ precache-manifest.6f8cc70e83fe0b8c22a054b6e5ea7a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0443cdd08a6c5063f8ea16b63ae3dc5",
+    "url": "/react-fun/index.html"
+  },
+  {
+    "revision": "2246376af86951e591a4",
+    "url": "/react-fun/static/css/main.9f8c65d4.chunk.css"
+  },
+  {
+    "revision": "6f993888e92e39335b88",
+    "url": "/react-fun/static/js/2.bc1401dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fun/static/js/2.bc1401dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2246376af86951e591a4",
+    "url": "/react-fun/static/js/main.33f4ab13.chunk.js"
+  },
+  {
+    "revision": "2501e9c07513975e758d",
+    "url": "/react-fun/static/js/runtime-main.c7b052cd.js"
+  }
+]);
